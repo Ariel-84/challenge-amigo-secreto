e517a0a7cb7a9444e5d6733ae9cef67c4d10c261
ariel-84 <ariel.caferri@hotmail.com>
2025-02-19 21:55:04 -0300

    Desarrollo-amigo-secreto-funciones

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let listaAmigos = [];
+
+function asignarTextoElemento (elemento, texto) {
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+}
+
+function agregarAmigo() {
+    let nuevoAmigo = document.getElementById('amigo').value.trim();
+    if (nuevoAmigo !== '') {
+      listaAmigos.push(nuevoAmigo);
+      document.getElementById('amigo').value = '';
+      actualizarListaAmigos();
+    } else {
+      alert('Ingresa un nombre válido');
+    } 
+  }
+    function actualizarListaAmigos() {
+        let listaElement = document.getElementById('listaAmigos');
+        listaElement.innerHTML = '';
+        for (let i = 0; i < listaAmigos.length; i++) {
+          let nuevoItem = document.createElement("li");
+          nuevoItem.textContent = listaAmigos[i];
+          listaElement.appendChild(nuevoItem);
+        }
+      }
+
+
+function sortearAmigo() {
+
+}
+
+asignarTextoElemento('h1','Amigo Secreto' );
+asignarTextoElemento('h2','Digite el nombre de sus amigos' );
