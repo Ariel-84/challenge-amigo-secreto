5a7385dde83add5a6ed0baa01faa7dacab16d4d1
ariel-84 <ariel.caferri@hotmail.com>
2025-02-28 16:32:10 -0300

    Funcion-sortear

--- app.js
+++ app.js
@@ -1,42 +1,56 @@
+// array lista de amigos.
 let listaAmigos = [];
 
-function asignarTextoElemento (elemento, texto) {
-    let elementoHTML = document.querySelector(elemento);
+// Función para asignar texto a un elemento HTML
+function asignarTextoElemento(elemento, texto) {
+    let elementoHTML = document.getElementById(elemento);
     elementoHTML.innerHTML = texto;
 }
-
+//Función para agregar un amigo a la lista
 function agregarAmigo() {
   let nuevoAmigo = document.getElementById('amigo').value.trim();
 
+//Condición para validar que el nombre no esté vacío o que ya haya sido agregado
   if (nuevoAmigo === '' || listaAmigos.includes(nuevoAmigo)) {
-      alert(nuevoAmigo === '' ? 'Ingresa un nombre válido' : 'Este nombre ya ha sido agregado.');
+      alert(nuevoAmigo === '' ? 'Ingresa un nombre valido' : 'Este nombre ya ha sido agregado.');
       return;
   }
-
+//Condición para validar que el nombre no sea un número
   if (!isNaN(nuevoAmigo) && nuevoAmigo !== '') {
     alert('Inserte un nombre, no un número.');
     return;
-}
+  }
+//Agregar el nuevo amigo a la lista
   listaAmigos.push(nuevoAmigo);
   document.getElementById('amigo').value = '';
   actualizarListaAmigos();
-
 }
-    function actualizarListaAmigos() {
-        let listaElement = document.getElementById('listaAmigos');
-        listaElement.innerHTML = '';
+//Función para actualizar la lista de amigos
+function actualizarListaAmigos() {
+    let listaElement = document.getElementById('listaAmigos');
+    listaElement.innerHTML = ''; // Limpiar la lista antes de mostrarla nuevamente
 
-        for (let i = 0; i < listaAmigos.length; i++) {
-          let nuevoItem = document.createElement("li");
-          nuevoItem.textContent = listaAmigos[i];
-          listaElement.appendChild(nuevoItem);
-        }
-      }
-
-
+    for (let i = 0; i < listaAmigos.length; i++) {
+      let nuevoItem = document.createElement("li");
+      nuevoItem.textContent = listaAmigos[i];
+      listaElement.appendChild(nuevoItem);
+    }
+}
+//Función para sortear un amigo
 function sortearAmigo() {
+  if (listaAmigos.length < 2) {  // Validar que haya al menos dos amigos
+    alert('Debe haber al menos dos amigos para realizar el sorteo.');
+    return;
+  }
 
+  // Realizar el sorteo
+  let indiceSorteado = Math.floor(Math.random() * listaAmigos.length);
+  let amigoSorteado = listaAmigos[indiceSorteado];
+
+  // Mostrar el resultado.
+  document.getElementById('resultado').textContent = `El amigo sorteado es: ${amigoSorteado}`;
 }
 
-asignarTextoElemento('h1','Amigo Secreto' );
-asignarTextoElemento('h2','Digite el nombre de sus amigos' );
+// Asignar títulos iniciales
+asignarTextoElemento('h1', 'Amigo Secreto');
+asignarTextoElemento('h2', 'Digite el nombre de sus amigos');
